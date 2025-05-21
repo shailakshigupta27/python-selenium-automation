73ebe68e61958fe5fd91ec207549b6858feb0fec
Shailakshi Gupta <shailakshigupta@Shailakshis-MacBook-Air.local>
2025-05-21 13:22:36 -0500

    Update search script

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 # get the path to the ChromeDriver executable
 driver_path = ChromeDriverManager().install()
 
@@ -18,7 +22,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Car')
+search.send_keys('Tree')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +31,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'Tree'.lower() in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
